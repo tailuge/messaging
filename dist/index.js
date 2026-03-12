d823f9c80663ab6c7a9a6e76c9ef791770849dc7
tailuge <tailuge@gmail.com>
2026-03-12 06:55:43 +0000

    tidy up

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./messagingclient";
-export * from "./lobby";
-export * from "./table";
-export * from "./types";
-export * from "./nchanclient";
-//# sourceMappingURL=index.js.map
--- dist/utils/uid.js
+++
@@ -1,4 +0,0 @@
-export function getUID() {
-    return "xxxxxxxx".replace(/x/g, () => Math.floor(Math.random() * 16).toString(16));
-}
-//# sourceMappingURL=uid.js.map
