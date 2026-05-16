c3752610528ff54c31aa0687397890eedd0c8dd1
tailuge <tailuge@gmail.com>
2026-05-16 19:57:06 +0100

    up

--- src/client/lobby.js
+++ src/client/lobby.js
@@ -6,7 +6,7 @@ import './online-panel.js';
 import './user-badge.js';
 import './settings-modal.js';
 
-const CLIENTVERSION = 135;
+const CLIENTVERSION = 136;
 
 class LobbyApp extends LitElement {
     static properties = { _theme: { type: String, reflect: true, attribute: 'theme' } };
