e69893da0f887852a2fcc85fd5ebc1dfdbf39c2b
tailuge <tailuge@gmail.com>
2026-08-21 20:25:58 +0100

    seo

--- src/client/utils.js
+++ src/client/utils.js
@@ -1,7 +1,7 @@
 
 import { html } from 'lit';
 
-export const CLIENTVERSION = 785;
+export const CLIENTVERSION = 786;
 export const formatVersion = (v) => `v${Math.floor(v / 100)}.${String(v % 100).padStart(2, '0')}`;
 
 
